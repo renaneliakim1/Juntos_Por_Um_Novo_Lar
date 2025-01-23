fdc46a899de3543c3dc040fb18bedc1fe3f1ca15
renaneliakim1 <renaneliakim1@gmail.com>
2025-01-22 21:04:57 -0300

    feat(all): add responsividade

---
+++ js/menu-toggle.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
+
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("show");
+    });
+});
